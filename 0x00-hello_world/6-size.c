06ca0f4041c8020cc9e77f28f65ac46ce94ce3c6
Arlene404 <giraarle080@gmail.com>
2022-04-10 19:49:39 -0700

    My 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("The size of a char: %d byte(s)\n", sizeof(c));
+printf("The size of an int: %d byte(s)\n", sizeof(a));
+printf("The size of long int: %d byte(s)\n", sizeof(b));
+printf("The size of long long int: %d byte(s)\n", sizeof(c);	
+printf("The size of a float: %d byte(s)\n", sizeof (f)):
+return (0);
+}
