a03f899e5825da8a13c89cc4ee4d546f9a799b1a
Arlene404 <giraarle080@gmail.com>
2022-04-14 19:16:23 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Print_number - Prints the number 0mto9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+i = 0;
+for (i= 0; i < 10; i++)
+
+_putchar(i + '0');
+
+_putchar('\n');
+}
