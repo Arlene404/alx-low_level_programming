b93fe07b70e5bfa4a2b71649d2f3fbf3273c279c
Arlene404 <giraarle080@gmail.com>
2022-04-18 07:53:18 -0700

    like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,15 +12,13 @@ printf("infinite loop incoming :(\n);
 
 i = 0;
 
-
 /*
  * while (i < 10)
  * {
- * infinite loop - No increase of variable
  * putchar(i);
-}
+ * }
  */
 
-printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided!: \\o/\n");
 return (0);
 }
